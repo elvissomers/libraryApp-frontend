ab2e9804bdcd22dd6558ab246ae5dfda740c17d6
Steash <62103289+Steash@users.noreply.github.com>
2023-03-06 18:11:30 +0100

    CRD functionality & mybookspage progress

--- bookstore-client/src/views/MyBooksView.vue
+++ bookstore-client/src/views/MyBooksView.vue
@@ -1,7 +1,31 @@
 <template>
     <div class="MyBooks">
-        My books page
-        <LoginCard></LoginCard>
+        <JimTest></JimTest>
+        <PostTest></PostTest>
+        <!-- <DeleteTest></DeleteTest> -->
+
+        <div>
+            <h1 class="font-bold">Book List Delete</h1>
+            <ul>
+                <li v-for="book in books" :key="book.id">
+                {{ book.title }} by {{ book.author }}
+                - {{ book.id }}
+                <button class="bg-black text-slate-50" 
+                @click="deleteBook(book.id)">Delete</button>
+                </li>
+            </ul>
+         </div>
+
+        <div class="flex flex-row">
+            <BookCard 
+            v-for="book in books" :key="book.id"
+            v-bind:title="book.title"
+            v-bind:author="book.author">
+            </BookCard>
+        </div>
+
+        
+
 
 
 
@@ -10,12 +34,52 @@
 
 <script>
 // @ is an alias to /src
-import LoginCard from '@/components/LoginCard.vue';
+import axios from 'axios';
+import JimTest from '@/components/lelijke-htmls/JimTest.vue';
+import PostTest from '@/components/lelijke-htmls/PostTest.vue';
+import BookCard from '@/components/BookCard.vue'
+// import DeleteTest from '@/components/lelijke-htmls/DeleteTest.vue';
 
 export default {
-  name: 'MyBooksView',
-  components: {
-    LoginCard
-}
+    name: 'MyBooksView',
+    components: {
+    JimTest,
+    BookCard,
+    PostTest,
+    // DeleteTest
+},
+    data() {
+      return {
+        books: [],
+      };
+    },
+    mounted() {
+      this.getBooks();
+    },
+    methods: {
+      getBooks() {
+        axios.get('http://localhost:8080/book')
+          .then(response => {
+            this.books = response.data;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      },
+      deleteBook(id) {
+        console.log(id)
+
+        axios.delete(`http://localhost:8080/book/${id}`)
+          .then(response => {
+            console.log('Book deleted:', response.data);
+            // Remove the deleted book from the books array
+            this.books = this.books.filter(book => book.id !== id);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      },
+    },
 }
 </script>
+
