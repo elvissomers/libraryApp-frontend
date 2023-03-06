44afe16ed48475ec8bff5c13352fa4649749d175
anwar <anwar.sakkali@student.hu.nl>
2023-03-06 14:40:44 +0100

    readme update and start api calls

--- bookstore-client/src/views/MyBooksView.vue
+++ bookstore-client/src/views/MyBooksView.vue
@@ -1,21 +1,42 @@
 <template>
     <div class="MyBooks">
         My books page
-        <LoginCard></LoginCard>
-
-
+        <h1 id="test">total vue packages: {{totalVuePackages}}</h1>
+        <MyBooksTest></MyBooksTest>
 
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import LoginCard from '@/components/LoginCard.vue';
+import MyBooksTest from '@/components/lelijke-htmls/MyBooksTest.vue'
+import axios from 'axios'
 
 export default {
   name: 'MyBooksView',
   components: {
-    LoginCard
+    MyBooksTest,
+    },
+
+    data() {
+        return {
+            totalVuePackages: null,
+            bookList: []
+            };
+    },
+
+    created(){
+        axios.get("http://localhost:8080/book")
+      .then(response => {
+        for(var title in response['data']){
+            for (var book in title){
+                console.log(book)
+                this.bookList.push(book)
+                this.totalVuePackages = book
+            }
+        }
+      })
+    }
 }
-}
+
 </script>
