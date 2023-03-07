adb29073800b7751db11efe87c6815642a64bc68
Anwar <anwar.sakkali.kk@gmail.com>
2023-03-07 15:56:12 +0100

    updated python database script and show person

--- bookstore-client/src/components/ViewPerson.vue
+++ bookstore-client/src/components/ViewPerson.vue
@@ -4,9 +4,9 @@
       <div v-if="user">
         <p><strong>ID:</strong> {{ user.id }}</p>
         <p><strong>Admin:</strong> {{ user.admin }}</p>
-        <p><strong>Email:</strong> {{ user.email }}</p>
-        <p><strong>First Name:</strong> {{ user.first_name }}</p>
-        <p><strong>Last Name:</strong> {{ user.last_name }}</p>
+        <p><strong>Email:</strong> {{ user.eMailAddress }}</p>
+        <p><strong>First Name:</strong> {{ user.firstName }}</p>
+        <p><strong>Last Name:</strong> {{ user.lastName }}</p>
       </div>
       <div v-else>
         <p>Loading user details...</p>
@@ -28,6 +28,7 @@
       axios.get(`http://localhost:8080/user/${id}`)
         .then(response => {
           this.user = response.data;
+          console.log(this.user)
         })
         .catch(error => {
           console.error(error);
