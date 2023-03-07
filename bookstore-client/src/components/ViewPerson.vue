85abf39d5974e802dda5205a2c6f669bc95ca810
Anwar <anwar.sakkali.kk@gmail.com>
2023-03-07 15:33:22 +0100

    database to assets and viewperson in page

--- bookstore-client/src/components/ViewPerson.vue
+++ bookstore-client/src/components/ViewPerson.vue
@@ -1,5 +1,38 @@
 <template>
-    <h1>Hello there</h1>
-    <br>
-    <h1>this is a test page to view your account details</h1>
-</template>
+    <div>
+      <h1>User Details</h1>
+      <div v-if="user">
+        <p><strong>ID:</strong> {{ user.id }}</p>
+        <p><strong>Admin:</strong> {{ user.admin }}</p>
+        <p><strong>Email:</strong> {{ user.email }}</p>
+        <p><strong>First Name:</strong> {{ user.first_name }}</p>
+        <p><strong>Last Name:</strong> {{ user.last_name }}</p>
+      </div>
+      <div v-else>
+        <p>Loading user details...</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        user: null
+      };
+    },
+    mounted() {
+      const id = 1;
+      axios.get(`http://localhost:8080/user/${id}`)
+        .then(response => {
+          this.user = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  };
+  </script>
+  
