008c08a571b852effee0b06ac6f8abf2f6c792ab
Anwar <anwar.sakkali.kk@gmail.com>
2023-03-09 15:26:11 +0100

    redundant line

--- bookstore-client/src/components/admin-panel/EditBooks.vue
+++ bookstore-client/src/components/admin-panel/EditBooks.vue
@@ -1,5 +1,4 @@
 <template>
-    <h1>Beheer boeken</h1>
     <button>
 
         <router-link to="/admin/add-book" class="flex items-center space-x-1 rounded-md px-2 py-3 border-neutral-900 border-2 hover:bg-gray-100 hover:text-blue-600">
