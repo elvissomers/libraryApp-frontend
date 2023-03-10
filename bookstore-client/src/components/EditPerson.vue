ff850b23aaf7f5dabc54255a98f1e05a25767737
Anwar <anwar.sakkali.kk@gmail.com>
2023-03-10 11:17:54 +0100

    user data can be changed

--- bookstore-client/src/components/EditPerson.vue
+++ bookstore-client/src/components/EditPerson.vue
@@ -2,11 +2,11 @@
     <section>
         <div class="form-box">
             <div class="form-value">
-                <form @submit.prevent="createUser">
-                    <h1>Creëer nieuwe gebruiker</h1>
+                <form @submit.prevent="saveUserChanges">
+                    <h1>Wijzig gebruiker gegevens</h1>
                     <div class="inputbox">
                         <!-- <ion-icon name="mail-outline"></ion-icon> -->
-                        <input type="email" id="email" v-model="user.eMailAddress" required>
+                        <input type="email" id="email" v-model="user.emailAddress" required>
                         <label for="">Email</label>
                     </div>
                     <div class="inputbox">
@@ -19,11 +19,12 @@
                         <input type="text" id="lastname" v-model="user.lastName" required>
                         <label for="">Achternaam</label>
                     </div>
-                    <div>
-                        <label for="">Admin:</label>
-                        <input type="checkbox" v-model="admin">
-                        </div>
-                    <button type="submit">Registreer</button>
+                    <div class="inputbox">
+                        <!-- <ion-icon name="lock-closed-outline"></ion-icon> -->
+                        <input type="password" id="password" v-model="user.password" required>
+                        <label for="">Wachtwoord</label>
+                    </div>
+                    <button type="submit">Sla wijzigingen op</button>
                 </form>
             </div>
         </div>
@@ -39,23 +40,38 @@ export default {
   data() {
     return {
       user: {
-        eMailAddress: '',
+        emailAddress: '',
         firstName: '',
         lastName: '',
+        password: '',
       },
     };
   },
+  created() {
+    axios.get('http://localhost:8080/user/1')
+      .then(response => {
+        this.user.firstName = response.data.firstName;
+        this.user.lastName = response.data.lastName;
+        this.user.emailAddress = response.data.emailAddress;
+        this.user.password = response.data.password;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
   methods: {
       
-    createUser() {
-      axios.post('http://localhost:8080/user/create', this.user)
+    saveUserChanges() {
+      axios.put('http://localhost:8080/user/1', this.user)
         .then(response => {
-          console.log('User created:', response.data);
+            console.log('User created:', response.data);
+            alert("Gebruiker succesvol gewijzigd!");
         })
         .catch(error => {
-          console.log(error);
+            alert('Er is iets fout gegaan')
+            console.log(error);
         })
-        .then(() => alert("Gebruiker toevoegen succesvol!"));
+        
     },
   },
 };
@@ -70,13 +86,13 @@ export default {
     }
     section{
         display: flex;
-        justify-content: top left;
-        align-items: top left;
+        justify-content: top;
+        align-items: top;
         min-height: 100vh;
         width: 100%;
         
         /* background: url('background6.jpg')no-repeat; */
-        background-position: left;
+        background-position: center;
         background-size: cover;
     }
     .form-box{
@@ -88,8 +104,8 @@ export default {
         border-radius: 20px;
         backdrop-filter: blur(15px);
         display: flex;
-        justify-content: left;
-        align-items: left;
+        justify-content: center;
+        align-items: center;
 
     }
     h2{
