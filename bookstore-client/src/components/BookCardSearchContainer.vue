59317750a0a3b8afd7d7475757661dbc7e3bc285
kabakker <koen_Az@hotmail.com>
2023-03-13 10:23:40 +0100

    Aligning in cards

--- bookstore-client/src/components/BookCardSearchContainer.vue
+++ bookstore-client/src/components/BookCardSearchContainer.vue
@@ -1,20 +1,15 @@
 <template>
     <div class="Catalogue">
         <!--<div class="flex flex-row flex-wrap justify-center">-->
-          <div class="mx-40 my-5 grid grid-cols-5 gap-2">
-            <BookCardCatalogue
-            v-for="book in books" :key="book.id"
-            v-bind:title="book.title" 
-            v-bind:author="book.author"
-            v-bind:isbn="book.isbn">
+        <div class="mx-40 my-5 grid grid-cols-4 gap-2">
+            <BookCardCatalogue v-for="book in books" :key="book.id" v-bind:title="book.title" v-bind:author="book.author"
+                v-bind:isbn="book.isbn">
             </BookCardCatalogue>
-          </div>
-      <PaginationBar 
-      v-bind:curPage="currentPage"    
-      @changePage="changePageNumber($event)">
-      </PaginationBar>
+        </div>
+        <PaginationBar v-bind:curPage="currentPage" @changePage="changePageNumber($event)">
+        </PaginationBar>
     </div>
-  </template>
+</template>
 
 <script>
 // @ is an alias to /src
@@ -23,64 +18,59 @@ import BookCardCatalogue from '@/components/BookCardCatalogue.vue'
 import PaginationBar from '@/components/PaginationBar.vue'
 
 export default {
-  name: 'BookCardSearchContainer',  
-  components: {
-    BookCardCatalogue,
-    PaginationBar},
-
-  props: ['searchTerm'],
-
-  watch: {
-    searchTerm: function() {
-        this.currentPage = 0
-        this.searchBooks()
-    }
-  },
-
-  data() {
-    return {
-      books: [],
-      currentPage: 0,
-      previousPage: 0,
-      startOver: this.startAgain
-    };
-  },
-
-  mounted() {
-    this.searchBooks();
-  },
-
-
-
-  methods: {
-    
-    searchBooks() { 
-
-      axios.get('http://localhost:8080/booksearch/'+this.searchTerm+'/'+this.currentPage+'/5')
-        .then(response => {  
-          if (response.data.length > 0) {
-            this.books = response.data;
-          }
-          else {
-            this.currentPage = this.previousPage
-          }  
-        })
-        .catch(error => {
-          console.log(error);
-        });
+    name: 'BookCardSearchContainer',
+    components: {
+        BookCardCatalogue,
+        PaginationBar
     },
 
-    changePageNumber(change) {
-        this.previousPage = this.currentPage
-        this.currentPage += change;
-        if (this.currentPage >= 0) {
+    props: ['searchTerm'],
+
+    watch: {
+        searchTerm: function () {
+            this.currentPage = 0
             this.searchBooks()
         }
-        else {
-            this.currentPage = this.previousPage
+    },
+
+    data() {
+        return {
+            books: [],
+            currentPage: 0,
+            previousPage: 0,
+            startOver: this.startAgain
+        };
+    },
+
+    mounted() {
+        this.searchBooks(0);
+    },
+
+
+
+    methods: {
+
+        searchBooks(pageNumber) {
+
+            axios.get('http://localhost:8080/booksearch/' + this.searchTerm + '/' + pageNumber + '/8')
+                .then(response => {
+                    if (response.data.length > 0) {
+                        this.books = response.data;
+                        this.currentPage = pageNumber
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+
+        changePageNumber(change) {
+            const tempPage = this.currentPage + change;
+            if (this.currentPage >= 0) {
+                this.searchBooks(tempPage)
+            }
         }
-      } 
-  }
-      
+    }
+
 }
 </script>
