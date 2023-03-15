ebd959d16b6add0129fa32c044f6266123f2de53
Steash <62103289+Steash@users.noreply.github.com>
2023-03-15 10:37:45 +0100

    added back buttons

---
+++ src/components/admin-panel/buttons/AddBookButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-lime-500  hover:bg-lime-600 rounded-md h-fit" title="Add book">
+        <router-link to="/admin/add-book">
+            <span class="material-symbols-outlined">add_circle</span>
+        </router-link>
+    </button>
+</template>
