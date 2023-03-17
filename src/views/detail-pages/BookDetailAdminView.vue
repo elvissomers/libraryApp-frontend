338ba173bcda00b2e723ce8acb6886ac836c4dc2
elvissomers <elvissomers98@gmail.com>
2023-03-17 10:58:12 +0100

    Create BookDetailAdminView.vue

---
+++ src/views/detail-pages/BookDetailAdminView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+
+        <BookDetailAdmin></BookDetailAdmin>
+  
+  
+    </div>
+  </template>
+  
+  <script>
+import BookDetailAdmin from '@/components/details/BookDetailAdmin.vue';
+  
+  export default {
+    name: 'HomeView',
+    components: {
+    BookDetailAdmin
+    },
+    mounted(){
+        // this.authenticateAdmin()
+    },
+    methods: {
+        authenticateAdmin() {
+        if (localStorage.getItem('token') == null || localStorage.getItem('admin') != 'true') {
+            this.$router.push('/book/user' + this.$route.params.id);
+            console.log('redirecting to login')
+        }
+    }
+
+  }
+}
+  </script>
+  
