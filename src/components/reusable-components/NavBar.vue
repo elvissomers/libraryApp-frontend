e7f3f9c38edf00d54abeb4737106db23a3ab547f
Steash <62103289+Steash@users.noreply.github.com>
2023-03-17 09:57:11 +0100

    Fixed merge bugs

--- src/components/reusable-components/NavBar.vue
+++ src/components/reusable-components/NavBar.vue
@@ -5,36 +5,41 @@
             <!-- navbar -->
             <nav class="flex justify-between bg-zinc-900 text-white w-screen">
                 <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-                <router-link to="/" class="text-3xl font-bold font-heading"><img src="https://www.workingtalent.nl/_nuxt/img/wt-logo-white.8cc9feb.svg" title="Logo" alt="Logo"></router-link>
-                <!-- Nav Links -->
-                <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-                    <li><router-link to="/" class="hover:text-gray-200">Catalogue</router-link></li>
-                    <li><router-link to="/mybooks" class="hover:text-gray-200">My Books</router-link></li>
-                    <li><router-link to="/contact" class="hover:text-gray-200">Contact</router-link></li>
-                    <li><router-link to="/admin/edit-users" class="hover:text-gray-200">Admin panel</router-link></li>         
-                </ul>
-                <!-- Account -->
-                <div class="hidden xl:flex items-center space-x-5 items-center">
-                    <!-- Sign In / Register      -->
-                    <router-link to="/myaccount" class="flex items-center hover:text-gray-200">My Account
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                    </router-link>
-                    
+                    <router-link to="/" class="text-3xl font-bold font-heading"><img
+                            src="https://www.workingtalent.nl/_nuxt/img/wt-logo-white.8cc9feb.svg" title="Logo"
+                            alt="Logo"></router-link>
+                    <!-- Nav Links -->
+                    <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+                        <li><router-link to="/" class="hover:text-gray-200">Catalogue</router-link></li>
+                        <li><router-link to="/mybooks" class="hover:text-gray-200">My Books</router-link></li>
+                        <li><router-link to="/contact" class="hover:text-gray-200">Contact</router-link></li>
+                        <li><router-link to="/admin/edit-users" class="hover:text-gray-200">Admin panel</router-link></li>
                         <li><router-link to="/" @click="logOut" class="flex items-center hover:text-gray-200">
-                                Log out
+                            Log out
+                        </router-link></li>
                     </ul>
+                    <!-- Account -->
+                    <div class="hidden xl:flex items-center space-x-5 items-center">
+                        <!-- Sign In / Register      -->
+                        <router-link to="/myaccount" class="flex items-center hover:text-gray-200">My Account
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </router-link>
+                    </div>
+                    <!-- Responsive navbar -->
+                    <a class="xl:hidden flex mr-6 items-center" href="#">
+                    </a>
+                    <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </a>
                 </div>
-                <!-- Responsive navbar -->
-                <a class="xl:hidden flex mr-6 items-center" href="#">
-                </a>
-                <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                </a>
             </nav>
 
         </section>
@@ -76,14 +81,14 @@ export default {
             }
 
             axios.put('http://localhost:8080/user/logout', null, config)
-            .then(response => {
-                console.log(1)
-                console.log('Token deleted:', response.data);
-            })
-            .catch(error => {
-                console.log(error);
-                console.log(2)
-            })
+                .then(response => {
+                    console.log(1)
+                    console.log('Token deleted:', response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                    console.log(2)
+                })
         },
     }
 }
--- src/views/admin-panel/user/EditUsersView.vue
+++ src/views/admin-panel/user/EditUsersView.vue
@@ -2,6 +2,7 @@
   <div class="EditUser flex flex-row">
     <SideBar></SideBar>
     <UserTable></UserTable>
+    <AddUserButton></AddUserButton>
   </div>
 </template>
 
@@ -9,6 +10,7 @@
 // @ is an alias to /src
 import SideBar from '@/components/reusable-components/SideBar.vue';
 import UserTable from '@/components/admin-panel/user-overview/UserTable.vue';
+import AddUserButton from '@/components/admin-panel/buttons/AddUserButton.vue';
 
 export default {
   name: 'EditUserView',
