46512d71d9a6d8f8d0d824476ac767ac36dcb461
kabakker <koen_Az@hotmail.com>
2023-03-20 16:28:55 +0100

    Leen uit

--- src/components/details/book-detail-page/UsersRowPopup.vue
+++ src/components/details/book-detail-page/UsersRowPopup.vue
@@ -9,7 +9,7 @@
     <div class="flex flex-row">
         <button
           class="float-right text-white bg-blue-500 px-4 py-2 m-2 h-fit rounded-md transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
-          @click="$emit('createLoanFromUser', user)">Bekijk
+          @click="$emit('createLoanFromUser', user)">Leen Uit
         </button>
   
     </div>
