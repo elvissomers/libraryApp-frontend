a1db24d1a5fe5bc6019c448f81663d69fe28e226
elvissomers <elvissomers98@gmail.com>
2023-03-20 10:50:24 +0100

    Added loan table/row
    Added loan table/row in book detail folder

---
+++ src/components/details/book-detail-page/BookLoanRow.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-row justify-between hover:bg-slate-300">
+      <div class="flex flex-row py-4">
+        <div class="w-36 ml-8">{{ loan.startDate }}</div>
+        <div class="w-36">{{ loan.userFirstName }}</div>
+        <div class="w-36">{{ loan.userLastName }}</div>
+        <div class="">{{ loan.bookTitle }}</div>
+      </div>
+    </div>
+  </template>
+      
+      
+  <script>
+  
+  export default {
+    name: "LoanRow",
+    props: ['loan'],
+  
+    methods: {
+    },
+  
+  }
+  
+  
+  </script>
+      
