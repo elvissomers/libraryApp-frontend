a5a98c049f11003e462f0a728e7ad9f6bcb806d6
elvissomers <elvissomers98@gmail.com>
2023-03-20 11:20:36 +0100

    Copied reservation row
    Copied reservation row in to book detail folder

---
+++ src/components/details/book-detail-page/BookReservationRow.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="flex flex-row justify-between hover:bg-slate-300">
+      <div class="flex flex-row py-4">
+        <div class="w-36 ml-8">{{ reservation.date }}</div>
+        <div class="w-36">{{ reservation.userFirstName }}</div>
+        <div class="w-36">{{ reservation.userLastName }}</div>
+        <div class="">{{ reservation.bookTitle }}</div>
+      </div>
+      <button v-on:click="createReservation()"
+        class="text-white float-right px-4 py-2 m-2 h-fit rounded-md w-48"
+        :class="[reservation.available ? 'bg-blue-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300' : 'bg-gray-400 cursor-not-allowed']"
+        :disabled="!reservation.available">
+        {{ reservation.available ? 'Goedkeuren!' : 'Niet Beschikbaar' }}
+      </button>
+    </div>
+  </template>
+      
+      
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: "ReservationRow",
+    props: ['reservation'],
+  
+  
+    methods: {
+      createReservation() {
+        let saveReservationDto = {}
+        saveReservationDto.copyNumber = prompt("Welk copy number wil je toekennen?")
+        saveReservationDto.startDate = new Date().getDate()
+        saveReservationDto.bookId = this.reservation.bookId
+        saveReservationDto.userId = this.reservation.userId
+        console.log(this.reservation)
+  
+        axios.post('http://localhost:8080/loan/create', saveReservationDto)
+          .then(response => {
+            console.log(response)
+            console.log('Copy added:', response.data);
+            alert("Exemplaar succesvol toegevoegd")
+          })
+          .catch(error => {
+            console.log(error);
+          })
+        axios.delete(`http://localhost:8080/reservation/delete/${this.reservation.id}`)
+          .then(response => {
+            console.log(response)
+            console.log('reservation deleted')
+            alert('reservation deleted')
+          })
+      },
+    },
+  
+  }
+  
+  
+  </script>
+  
