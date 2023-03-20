a4ebc0ab98677d8b1a93e79415ae313236cdd645
elvissomers <elvissomers98@gmail.com>
2023-03-20 12:32:01 +0100

    Added bookcopy
    Added bookcopy row and table to view all copies of a book

---
+++ src/components/details/book-detail-page/BookCopyRow.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-row justify-between hover:bg-slate-300">
+      <div class="flex flex-row py-4">
+        <div class="w-36 ml-8">{{ copy.number }}</div>
+        <div class="w-36 ml-8">{{ copy.available }}</div>
+      </div>
+      <!-- <button v-on:click="createLoan()"
+        class="text-white float-right px-4 py-2 m-2 h-fit rounded-md w-48"
+        :class="[reservation.available ? 'bg-blue-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300' : 'bg-gray-400 cursor-not-allowed']"
+        :disabled="!reservation.available">
+        {{ reservation.available ? 'Goedkeuren!' : 'Niet Beschikbaar' }}
+      </button> -->
+    </div>
+  </template>
+      
+      
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: "CopyRow",
+    props: ['copy']
+  
+  }
+  
+  
+  </script>
+  
