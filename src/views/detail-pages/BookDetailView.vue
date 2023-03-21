a635144385fe2302800219a7dfc1855567e7ac30
elvissomers <elvissomers98@gmail.com>
2023-03-21 13:44:45 +0100

    Revert "Update BookDetailView.vue"
    This reverts commit 306850e40816f5ee82f20b114235675f5e4e32e7.

--- src/views/detail-pages/BookDetailView.vue
+++ src/views/detail-pages/BookDetailView.vue
@@ -1,37 +1,33 @@
 <template>
-    <div class="home my-20">
+  <div class="home my-20">
 
-        <!-- <BookDetailAdmin></BookDetailAdmin> -->
-        <!-- // TODO : use either bookdetail or BookDetailAdmin
+    <!-- <BookDetailAdmin></BookDetailAdmin> -->
+    <!-- // TODO : use either bookdetail or BookDetailAdmin
         // Depending on token! -->
-        <BookDetail></BookDetail>
+    <BookDetail></BookDetail>
+
+
+  </div>
+</template>
   
-  
-    </div>
-  </template>
-  
-  <script>
+<script>
 import BookDetail from '@/components/details/BookDetail.vue';
-// import BookDetailAdmin from '@/components/details/BookDetailAdmin.vue';
-  
-  export default {
-    name: 'HomeView',
-    components: {
+import { store } from '@/store/store'
+
+export default {
+  name: 'HomeView',
+  components: {
     // BookDetailAdmin,
     BookDetail
   },
-  mounted(){
-        this.authenticateAdmin()
-    },
-    methods: {
-        authenticateAdmin() {
-        if (localStorage.getItem('admin') == 'true') {
-          // this.$router.push('/admin/edit-users')
-            this.$router.push('/book/admin/' + this.$route.params.id);
-            console.log('redirecting to admin view')
-        }
-    }
-  }
-  }
-  </script>
+  data() {
+    return {
+      store
+    };
+  },
+  mounted() {
+    this.store.authenticate(this.$router)
+  },
+}
+</script>
   
