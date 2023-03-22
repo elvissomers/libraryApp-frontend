3ffc343eae5462aaa93b631b025175120dd91c7f
Koen <koen_az@hotmail.com>
2023-03-22 12:32:37 +0100

    show past loans

--- src/components/admin-panel/loan-overview/LoanRow.vue
+++ src/components/admin-panel/loan-overview/LoanRow.vue
@@ -2,6 +2,7 @@
   <div class="flex flex-row justify-between hover:bg-slate-300">
     <div class="flex flex-row py-4">
       <div class="w-36 ml-8">{{ loan.startDate }}</div>
+      <div class="w-36">{{ loan.endDate }}</div>
       <div class="w-36">{{ loan.userFirstName }}</div>
       <div class="w-36">{{ loan.userLastName }}</div>
       <div class="">{{ loan.bookTitle }}</div>
