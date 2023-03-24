8be900a4531e7ec9cce3d58df10994e1f826920c
Koen <koen_az@hotmail.com>
2023-03-24 11:00:31 +0100

    Prompts in bootdetailadmin

--- src/components/details/BookDetailAdmin.vue
+++ src/components/details/BookDetailAdmin.vue
@@ -1,36 +1,52 @@
 <template>
   <section class="text-gray-700 body-font overflow-hidden">
-    <div class="container px-5 pt-16 mx-auto">
-      <div class="lg:w-4/5 mx-auto flex flex-wrap justify-center">
-        <img v-if="!bookFetching" alt="ecommerce" class="h-96 rounded border border-gray-200"
+    <div class="container mx-auto">
+      <div class="lg:w-full mx-auto flex flex-wrap justify-center">
+        <img v-if="!bookFetching" alt="ecommerce" class="h-96 rounded border border-gray-200 my-auto"
           v-bind:src="require(`@/assets/bookCovers/` + book.isbn + `.jpg`)">
         <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
           <span v-if="!bookFetching" class="text-sm title-font text-gray-500 mr-2 tracking-widest">{{ book.author
           }}</span>
 
-          <button @click="changeAuthor()"
+          <button @click="showPrompt('author')"
             class="text-white bg-grey-500 border-0 py-1 px-1 focus:outline-none hover:bg-grey-600 rounded">
             <img alt="ecommerce" class="lg:w-1/1 w-4 object-cover object-center rounded"
               src="https://cdn-icons-png.flaticon.com/512/61/61456.png?w=740&t=st=1678887410~exp=1678888010~hmac=91d3144990e622b8cafc0f2e8f4123973c3327bf4b075f343aaec0581930ca12">
           </button>
+          <PromptComponent v-bind:book="book" v-bind:typePrompt="'author'" class=""
+      :class="[prompts.author ? 'visible' : 'invisible']" @closePrompt="prompts.author = false"
+      @saveField="changeField('author', $event)"></PromptComponent>
 
           <h1></h1>
           <span v-if="!bookFetching" class="text-gray-900 text-3xl mr-2 title-font font-medium mb-1">{{ book.title
           }}</span>
 
-          <button @click="changeTitle()"
+          <button @click="showPrompt('title')"
             class="text-white bg-grey-500 border-0 py-1 px-1 focus:outline-none hover:bg-grey-600 rounded">
             <img alt="ecommerce" class="lg:w-1/1 w-4 object-cover object-center rounded"
               src="https://cdn-icons-png.flaticon.com/512/61/61456.png?w=740&t=st=1678887410~exp=1678888010~hmac=91d3144990e622b8cafc0f2e8f4123973c3327bf4b075f343aaec0581930ca12">
           </button>
+          <PromptComponent v-bind:book="book" v-bind:typePrompt="'title'" class=""
+      :class="[prompts.title ? 'visible' : 'invisible']" @closePrompt="prompts.title = false"
+      @saveField="changeField('title', $event)"></PromptComponent>
+
+
           <div class="flex mb-4">
           </div>
           <!-- Description -->
-          <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia
-            microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan
-            poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed
-            umami cardigan.</p>
-          <button>Edit</button>
+          <p v-if="!bookFetching" class="leading-relaxed h-64 overflow-y-auto border-4 p-4 border-slate-200">{{ book.description }}</p>
+          <button @click="showPrompt('description')"
+            class="text-white bg-grey-500 border-0 py-1 px-1 focus:outline-none hover:bg-grey-600 rounded">
+            <img alt="ecommerce" class="lg:w-1/1 w-4 object-cover object-center rounded"
+              src="https://cdn-icons-png.flaticon.com/512/61/61456.png?w=740&t=st=1678887410~exp=1678888010~hmac=91d3144990e622b8cafc0f2e8f4123973c3327bf4b075f343aaec0581930ca12">
+          </button>
+
+          <PromptComponent v-bind:book="book" v-bind:typePrompt="'description'" class=""
+      :class="[prompts.description ? 'visible' : 'invisible']" @closePrompt="prompts.description = false"
+      @saveField="changeField('description', $event)"></PromptComponent>
+
+
+
           <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
             <div class="flex">
               <span v-if="!bookFetching" class="mr-3">ISBN: {{ book.isbn }}</span>
@@ -61,13 +77,13 @@
   
 <script>
 import axios from 'axios';
-
+import PromptComponent from '@/components/reusable-components/PromptComponent.vue'
 
 export default {
   name: 'MyBooksView',
 
   components: {
-
+    PromptComponent
   },
 
   data() {
@@ -92,7 +108,8 @@ export default {
         amount: 1
         // ?
         // bookId: this.book.id
-      }
+      },
+      prompts: {description: false, title: false, author: false}
     };
   },
   mounted() {
@@ -121,6 +138,7 @@ export default {
     getBook() {
       axios.get('http://localhost:8080/book/get/' + this.$route.params.id)
         .then(response => {
+          console.log(response.data)
           this.book = response.data;
           this.copy.bookId = this.book.id;
           this.bookFetching = false
@@ -188,39 +206,6 @@ export default {
     },
 
 
-
-    // Change methods from here
-    changeAuthor() {
-      let headers = {
-        'Authentication': localStorage.getItem('token')
-      }
-
-      let newAuthor = prompt("Voer nieuwe auteur in:")
-
-      let changeBook = this.book
-      changeBook.author = newAuthor
-
-
-      axios.put('http://localhost:8080/book/update/' + this.$route.params.id, changeBook, {
-        headers: headers
-      })
-      console.log('Changed author name into ', newAuthor)
-
-    },
-
-    changeTitle() {
-      let headers = {
-        'Authentication': localStorage.getItem('token')
-      }
-      let newTitle = prompt("Voer nieuwe titel in:")
-      let changeBook = this.book
-      changeBook.title = newTitle
-      axios.put('http://localhost:8080/book/update/' + this.$route.params.id, changeBook, {
-        headers: headers
-      })
-      console.log('Changed title into ', newTitle)
-    },
-
     changeIsbn() {
       let newIsbn = prompt("Voer nieuw ISBN in:")
       let changeBook = this.book
@@ -229,6 +214,21 @@ export default {
       alert("ISBN veranderen niet toegestaan!")
       // axios.put('http://localhost:8080/book/' + this.$route.params.id, changeBook)
       // console.log('Changed isbn into ', newIsbn)
+    },
+
+    showPrompt(typePrompt) {
+      this.prompts[typePrompt] = true
+    },
+
+    changeField(whichField, newFieldValue) {
+      let headers = {
+        'Authentication': localStorage.getItem('token')
+      }
+      let changeBook = this.book
+      changeBook[whichField] = newFieldValue
+      axios.put('http://localhost:8080/book/update/' + this.$route.params.id, changeBook, {
+        headers: headers
+      })
     }
 
 
---
+++ src/components/reusable-components/PromptComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="fixed z-10 inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div class="flex flex-col w-fit bg-white p-8 rounded-3xl fixed top-32 border-8 border-green-600">
+
+            <div class="w-full mb-4">
+            <button v-on:click="this.$emit('closePrompt')" type="button" class="float-right">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+        Voer een nieuwe {{typePrompt}} in
+
+            <input
+                class="shadow appearance-none border rounded w-96 p-4 my-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                v-model="fieldValue" :placeholder=typePrompt>
+
+            <button
+                class="w-48 self-center text-white bg-blue-500 px-4 py-2 m-2 h-fit rounded-md transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+                @click="$emit('saveField', fieldValue); $emit('closePrompt')">Save
+            </button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+    name: 'PromptComponent',
+    components: {
+
+    },
+
+    props: ['book', 'typePrompt'],
+
+    data() {
+        return {
+            fieldValue: ''
+
+        };
+    },
+    mounted() {
+
+    },
+    methods: {
+
+
+    },
+}
+</script>
