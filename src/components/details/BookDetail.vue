f4e93061d744ac51c8a1aa6b357ee8c1665a183e
Steash <62103289+Steash@users.noreply.github.com>
2023-03-28 12:53:15 +0200

    design changes

--- src/components/details/BookDetail.vue
+++ src/components/details/BookDetail.vue
@@ -13,10 +13,8 @@
           <div class="flex mb-4">
           </div>
           <!-- Description -->
-          <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia
-            microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan
-            poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed
-            umami cardigan.</p>
+          <p v-if="!bookFetching" class="leading-relaxed h-64 overflow-y-auto border-y-2 p-1 my-2 border-slate-400">{{
+            book.description }}</p>
           <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
             <div class="flex">
               <span v-if="!bookFetching" class="mr-3">ISBN: {{ book.isbn }}</span>
